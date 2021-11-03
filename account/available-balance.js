bbd937dadf54016b6f13d434ae18a6815e0da497
orbitlens <orbit.lens@gmail.com>
2021-11-03 16:44:23 +0200

    Available balance helper

---
+++ account/available-balance.js
@@ -0,0 +1,21 @@
+import Bignumber from 'bignumber.js'
+
+/**
+ * Calculate available balance for a given account balance trustline
+ * @param {AccountResponse} account
+ * @param {Horizon.BalanceLine} balance
+ * @param {Number} [additionalReserves]
+ * @return {String}
+ */
+export function calculateAvailableBalance(account, balance, additionalReserves = null) {
+    let available = new Bignumber(balance.balance).minus(new Bignumber(balance.selling_liabilities))
+    if (balance.asset_type === 'native') {
+        const reserves = 2 + account.subentry_count + account.num_sponsoring - account.num_sponsored
+        available = available.minus(new Bignumber(reserves).mul(new Bignumber(0.5)))
+        //TODO: fetch base_reserve from the Horizon
+    }
+    if (additionalReserves !== null) {
+        available = available.minus(new Bignumber(additionalReserves))
+    }
+    return available.toString()
+}

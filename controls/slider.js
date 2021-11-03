1655c812831ddfa603f9f2ed1d2914bb91b68d47
orbitlens <orbit.lens@gmail.com>
2021-11-03 16:41:15 +0200

    Slider control

---
+++ controls/slider.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import {debounce} from 'throttle-debounce'
+import './slider.scss'
+
+const throttledUpdateSlippage = debounce(400, (callback, value) => callback(value))
+
+export function Slider({title, value, max, step = 1, onChange, ...otherProps}) {
+    const [innerValue, setInnerValue] = useState(value)
+    function change(e) {
+        let v = e.target.value
+        if (typeof v === 'string') {
+            v = parseFloat(v.replace(/[^\d.]/g, '')) || 0
+            if (v >= 99) {
+                v = 99
+            }
+        }
+        setInnerValue(v)
+        throttledUpdateSlippage(onChange, v)
+    }
+
+    return <div className="slider dual-layout dimmed text-small" {...otherProps}>
+        <div>{title}</div>
+        <div>
+            <input type="range" min={0} max={max} step={step} value={innerValue} onChange={change}/>
+        </div>
+        <div>
+            <input type="text" value={innerValue} onChange={change}/>%
+        </div>
+    </div>
+}

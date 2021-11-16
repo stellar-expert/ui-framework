a333f26362010cd0b150fabb4bd27265d86654c0
orbitlens <orbit.lens@gmail.com>
2021-11-16 12:19:11 +0200

    Extend identicon drawing capabilities

--- account/identicon.js
+++ account/identicon.js
@@ -36,28 +36,40 @@ function decodeBase32(input) {
     return buf
 }
 
-function drawIdenticon(canvas, stellarAddress) {
+/**
+ * Draw Stellar address identicon on the given canvas
+ * @param {HTMLCanvasElement} canvas
+ * @param {String} stellarAddress
+ * @param {Number} [size]
+ * @param {{top: Number, left: number}} [offset]
+ */
+export function drawIdenticon(canvas, stellarAddress, size = null, offset = null) {
     if (!canvas || !stellarAddress || !canvas.getContext || !canvas.width || !canvas.height) return
     const ctx = canvas.getContext('2d')
     if (!ctx) return
-
+    if (!offset) {
+        offset = {top: 0, left: 0}
+    }
+    if (size === null) {
+        size = canvas.width
+    }
     //take 16 meaningful bytes from the raw pub key
     const decoded = decodeBase32(stellarAddress).slice(2, 16),
         width = DEFAULT_SIZE,
         height = DEFAULT_SIZE,
         columns = Math.ceil(width / 2),
-        cellSize = canvas.width / width,
+        cellSize = size / width,
         addressBytes = decoded.slice(1)
 
     ctx.fillStyle = `hsl(${360 * decoded[0] / 256 | 0},58%,52%)`
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(offset.left, offset.top, size, size)
     for (let row = 0; row < height; row++) {
         for (let column = 0; column < columns; column++) {
             const position = column + row * columns,
                 bitSet = (addressBytes[position / 8 | 0] & (1 << (7 - position % 8))) !== 0
             if (bitSet) {
-                ctx.fillRect(cellSize * column, cellSize * row, cellSize, cellSize)
-                ctx.fillRect(cellSize * (width - column - 1), cellSize * row, cellSize, cellSize)
+                ctx.fillRect(offset.left + cellSize * column, offset.top + cellSize * row, cellSize, cellSize)
+                ctx.fillRect(offset.left + cellSize * (width - column - 1), offset.top + cellSize * row, cellSize, cellSize)
             }
         }
     }

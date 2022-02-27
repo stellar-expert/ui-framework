a6f4e78c4dc0193d2a89274c6e8d0eac0c477766
orbitlens <orbit.lens@gmail.com>
2022-02-27 14:02:28 +0200

    Fix LP assets order for alphanum4 + alphanum12 assets case

--- liquidity-pool/liquidity-pool-id.js
+++ liquidity-pool/liquidity-pool-id.js
@@ -22,12 +22,19 @@ export function getLiquidityPoolAsset(assets) {
     if (assets[0] === assets[1]) return null //invalid pool
     const wrappedAssets = assets.map(a => AssetDescriptor.parse(a).toAsset())
     wrappedAssets.sort((a, b) => {
+        //one of assets is XLM
         if (!a.issuer) return -1
         if (!b.issuer) return 1
+        //alphanum4 + alphanum12
+        if (a.code.length <= 4 && b.code.length > 4) return -1
+        if (b.code.length <= 4 && a.code.length > 4) return 1
+        //asset codes comparison
         if (a.code < b.code) return -1
         if (a.code > b.code) return 1
+        //issuers comparison when a.code == b.code
         if (a.issuer < b.issuer) return -1
         if (a.issuer > b.issuer) return 1
+        //identical
         return 0
     })
     return new LiquidityPoolAsset(wrappedAssets[0], wrappedAssets[1], LiquidityPoolFeeV18)

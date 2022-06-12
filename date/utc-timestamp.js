9d932f32143115f1e1bb2bd4eff27b7ae5cca422
orbitlens <orbit.lens@gmail.com>
2022-06-12 16:58:16 +0300

    Fix missing UtcTimestamp reference

--- date/utc-timestamp.js
+++ date/utc-timestamp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import cn from 'classnames'
 import {formatDateUTC} from '@stellar-expert/formatter'
 import {BlockSelect} from '../interaction/block-select'

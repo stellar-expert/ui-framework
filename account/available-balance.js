5612f6a5ceb0ec887c12de4ad0913d0ef00c3368
orbitlens <orbit.lens@gmail.com>
2022-08-16 02:49:48 +0300

    Fix Bignumber.js package upgrade issues

--- account/available-balance.js
+++ account/available-balance.js
@@ -11,7 +11,7 @@ export function calculateAvailableBalance(account, balance, additionalReserves =
     let available = new Bignumber(balance.balance).minus(new Bignumber(balance.selling_liabilities))
     if (balance.asset_type === 'native') {
         const reserves = 2 + account.subentry_count + account.num_sponsoring - account.num_sponsored
-        available = available.minus(new Bignumber(reserves).mul(new Bignumber(0.5)))
+        available = available.minus(new Bignumber(reserves).times(new Bignumber(0.5)))
         //TODO: fetch base_reserve from the Horizon
     }
     if (additionalReserves !== null) {
--- horizon/horizon-generic-id.js
+++ horizon/horizon-generic-id.js
@@ -17,8 +17,8 @@ import Bignumber from 'bignumber.js'
 export function parseStellarGenericId(id) {
     if (!/^\d{1,19}$/.test(id)) return {type: 'unknown'}
     let parsed = new Bignumber(id),
-        ledger = parsed.div(4294967296).floor(),
-        opOrder = parsed.modulo(4096)
+        ledger = parsed.idiv(4294967296),
+        opOrder = parsed.mod(4096)
 
     const res = {
         id,

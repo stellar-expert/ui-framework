7f42a7bfedd155482fd18987f02f4b4f5f0a072e
orbitlens <orbit.lens@gmail.com>
2023-07-26 17:13:04 +0300

    Unified atomic Slider component with categories

--- controls/slider.js
+++ controls/slider.js
@@ -1,30 +1,18 @@
-import React, {useState} from 'react'
-import {debounce} from 'throttle-debounce'
+import React, {useMemo} from 'react'
+import {throttle} from 'throttle-debounce'
 import './slider.scss'
 
-const throttledUpdateSlippage = debounce(400, (callback, value) => callback(value))
+export function Slider({value, max = 100, step = 1, categroies, onChange, ...otherProps}) {
+    const change = useMemo(() => throttle(400, e => onChange(parseFloat(e.target.value))), [onChange])
 
-export function Slider({title, value, max, step = 1, onChange, ...otherProps}) {
-    const [innerValue, setInnerValue] = useState(value)
-    function change(e) {
-        let v = e.target.value
-        if (typeof v === 'string') {
-            v = parseFloat(v.replace(/[^\d.]/g, '')) || 0
-            if (v >= 99) {
-                v = 99
-            }
-        }
-        setInnerValue(v)
-        throttledUpdateSlippage(onChange, v)
-    }
-
-    return <div className="slider dual-layout dimmed text-small" {...otherProps}>
-        <div>{title}</div>
+    return <div className="slider dimmed text-small" {...otherProps}>
         <div>
-            <input type="range" min={0} max={max} step={step} value={innerValue} onChange={change} data-lpignore="true"/>
-        </div>
-        <div>
-            <input type="text" value={innerValue} onChange={change}/>%
+            <input type="range" min={0} max={max} step={step} value={value} onChange={change}/>
+            {categroies && <datalist className="categories">
+                {categroies.map((category, index) =>
+                    <option key={category} value={index + category} label={category} className="dimmed condensed text-tiny"/>
+                )}
+            </datalist>}
         </div>
     </div>
 }

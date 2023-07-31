7de3dd83402d6e30bbc9ed9d46f1ebd7b71c27a2
orbitlens <orbit.lens@gmail.com>
2023-07-31 16:32:30 +0300

    Improve slider responsiveness

--- controls/slider.js
+++ controls/slider.js
@@ -1,13 +1,19 @@
-import React, {useMemo} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 import {throttle} from 'throttle-debounce'
 import './slider.scss'
 
-export function Slider({value, max = 100, step = 1, categroies, onChange, ...otherProps}) {
-    const change = useMemo(() => throttle(400, e => onChange(parseFloat(e.target.value))), [onChange])
+export function Slider({value, categroies, onChange, min = 0, max = 100, step = 1, ...otherProps}) {
+    const [inputValue, setInputValue] = useState(value || min)
+    const change = useMemo(() => throttle(300, onChange), [onChange])
+    const onSlide = useCallback(function (e) {
+        const value = parseFloat(e.target.value)
+        setInputValue(value)
+        change(value)
+    }, [change])
 
     return <div className="slider dimmed text-small" {...otherProps}>
         <div>
-            <input type="range" min={0} max={max} step={step} value={value} onChange={change}/>
+            <input type="range" min={min} max={max} step={step} value={inputValue} onChange={onSlide}/>
             {categroies && <datalist className="categories">
                 {categroies.map((category, index) =>
                     <option key={category} value={index + category} label={category} className="dimmed condensed text-tiny"/>

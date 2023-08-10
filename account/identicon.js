42f69802d91d9af7ccc77e4366cd0fe8c7b34b2c
orbitlens <orbit.lens@gmail.com>
2023-08-10 20:41:22 +0300

    Implement SVG-based identicons

--- account/identicon.js
+++ account/identicon.js
@@ -1,10 +1,12 @@
-import React, {useRef, useEffect} from 'react'
+import React from 'react'
 import './identicon.scss'
 
-const DEFAULT_SIZE = 7
-const base32Alphabet = {};
+const RESOLUTION = 7
+const SIZE = 448
+const base32Alphabet = {}
+
 //map base32 alphabet
-('ABCDEFGHIJKLMNOPQRSTUVWXYZ234567').split('').map((c, i) => base32Alphabet[c] = i)
+'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'.split('').map((c, i) => base32Alphabet[c] = i)
 
 function decodeBase32(input) {
     const buf = []
@@ -38,46 +40,32 @@ function decodeBase32(input) {
 
 /**
  * Draw Stellar address identicon on the given canvas
- * @param {HTMLCanvasElement|CanvasRenderingContext2D} canvas - Canvas to render on
  * @param {String} address - StrKey-encoded account address
  * @param {Number} size? - Identicon size
- * @param {{top: Number, left: number}} offset? - Drawing offset
+ * @return {String}
  */
-export function drawIdenticon(canvas, address, size = null, offset = null) {
-    if (!canvas || !address)
-        return
-    if (canvas?.getContext) {
-        canvas = canvas.getContext('2d')
-    }
-    if (!(canvas instanceof CanvasRenderingContext2D) || !canvas.canvas.width)
-        return
-    if (!offset) {
-        offset = {top: 0, left: 0}
-    }
-    if (size === null) {
-        size = canvas.canvas.width
-    }
+export function drawIdenticon(address, size = SIZE) {
     //take 16 meaningful bytes from the raw pub key
     const decoded = decodeBase32(address).slice(2, 16)
-    const width = DEFAULT_SIZE
-    const height = DEFAULT_SIZE
+    const width = RESOLUTION
+    const height = RESOLUTION
     const columns = Math.ceil(width / 2)
     const cellSize = size / width
     const addressBytes = decoded.slice(1)
-
-    canvas.fillStyle = `hsl(${360 * decoded[0] / 256 | 0},58%,52%)`
-    canvas.clearRect(offset.left, offset.top, size, size)
-
+    const fillStyle = `hsl(${360 * decoded[0] / 256 | 0},58%,52%)`
+    const dots = []
+    const rsize = ` width="${cellSize}" height="${cellSize}"`
     for (let row = 0; row < height; row++) {
         for (let column = 0; column < columns; column++) {
-            const position = column + row * columns,
-                bitSet = (addressBytes[position / 8 | 0] & (1 << (7 - position % 8))) !== 0
+            const position = column + row * columns
+            const bitSet = (addressBytes[position / 8 | 0] & (1 << (7 - position % 8))) !== 0
             if (bitSet) {
-                canvas.fillRect(offset.left + cellSize * column, offset.top + cellSize * row, cellSize, cellSize)
-                canvas.fillRect(offset.left + cellSize * (width - column - 1), offset.top + cellSize * row, cellSize, cellSize)
+                dots.push(`<rect x="${cellSize * column}" y="${cellSize * row}"${rsize}/>`)
+                dots.push(`<rect x="${cellSize * (width - column - 1)}" y="${cellSize * row}"${rsize}/>`)
             }
         }
     }
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${size} ${size}" fill="${fillStyle}">${dots.join('')}</svg>`
 }
 
 /**
@@ -87,14 +75,11 @@ export function drawIdenticon(canvas, address, size = null, offset = null) {
  * @constructor
  */
 export const AccountIdenticon = React.memo(function AccountIdenticon({address, size}) {
-    const canvas = useRef(null)
-    useEffect(() => drawIdenticon(canvas.current, address), [address])
-
     const props = {
         className: 'identicon',
-        ref: canvas,
-        width: 448,
-        height: 448
+        src: 'data:image/svg+xml;charset=utf-8,' + drawIdenticon(address),
+        width: SIZE,
+        height: SIZE
     }
     if (size) {
         props.style = {
@@ -102,5 +87,5 @@ export const AccountIdenticon = React.memo(function AccountIdenticon({address, s
             height: size
         }
     }
-    return <canvas {...props}/>
+    return <img {...props}/>
 })

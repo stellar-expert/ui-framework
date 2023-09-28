6f695f6da9ccac176cc5a06a3893c42836252586
orbitlens <orbit.lens@gmail.com>
2023-09-28 18:53:47 +0300

    Correctly denominate stroops in fee charge effect

--- tx/tx-fee-effect.js
+++ tx/tx-fee-effect.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Amount} from '../asset/amount'
+import {fromStroops} from '@stellar-expert/formatter'
 
 export default function TxFeeEffect({feeEffect, compact}) {
     const {charged, bid, bump} = feeEffect
     if (charged)
         return <div>
-            <Amount amount={charged} asset="XLM" issuer={!compact}/> transaction {!!bump && 'bump'} fee charged
+            <Amount amount={fromStroops(charged)} asset="XLM" issuer={!compact}/> transaction {!!bump && 'bump'} fee charged
         </div>
     return <div>
-        Charge transaction {!!bump && 'bump'} fee <Amount amount={bid} asset="XLM" issuer={!compact}/>
+        Charge transaction {!!bump && 'bump'} fee <Amount amount={fromStroops(bid)} asset="XLM" issuer={!compact}/>
     </div>
 }

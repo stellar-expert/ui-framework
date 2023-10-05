ad364b86e61135ac5c2ab8a85ad26142dbf2e14b
orbitlens <orbit.lens@gmail.com>
2023-10-05 22:43:02 +0000

    Add Rust syntax highlighting

--- controls/code-block.js
+++ controls/code-block.js
@@ -4,16 +4,19 @@ import cn from 'classnames'
 import hljs from 'highlight.js/lib/core'
 import jsLang from 'highlight.js/lib/languages/javascript'
 import jsonLang from 'highlight.js/lib/languages/json'
-import htmlLang from 'highlight.js/lib/languages/xml'
+import xmlLang from 'highlight.js/lib/languages/xml'
 import plaintextLang from 'highlight.js/lib/languages/plaintext'
 import tomlLang from 'highlight.js/lib/languages/ini'
+import rustLang from 'highlight.js/lib/languages/rust'
 import './code-block.scss'
 
 hljs.registerLanguage('js', jsLang)
 hljs.registerLanguage('json', jsonLang)
-hljs.registerLanguage('html', htmlLang)
+hljs.registerLanguage('html', xmlLang)
+hljs.registerLanguage('xml', xmlLang)
 hljs.registerLanguage('plain', plaintextLang)
 hljs.registerLanguage('toml', tomlLang)
+hljs.registerLanguage('rust', rustLang)
 
 export const CodeBlock = React.memo(function CodeBlock({children, lang, className, style}) {
     if (lang) {
@@ -26,7 +29,7 @@ export const CodeBlock = React.memo(function CodeBlock({children, lang, classNam
 
 CodeBlock.propTypes = {
     children: PropTypes.string.isRequired,
-    lang: PropTypes.oneOf(['js', 'json', 'html', 'plain', 'toml']),
+    lang: PropTypes.oneOf(['js', 'json', 'html', 'xml', 'toml', 'rust', 'plain']),
     className: PropTypes.string,
     style: PropTypes.object
 }

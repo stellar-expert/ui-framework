e86a79b6c82c5578872664ddaa3751823d0ccf72
orbitlens <orbit.lens@gmail.com>
2023-11-16 13:13:35 -0100

    Rewrite `useOnScreen` hook logic

--- state/on-screen-hooks.js
+++ state/on-screen-hooks.js
@@ -1,23 +1,21 @@
 import {useEffect, useState} from 'react'
 
-export function useOnScreen(ref, rootMargin, threshold) {
-    const [isIntersecting, setIntersecting] = useState(false)
+/**
+ * Hook for determining the visibility of the element inside the scroll context using the Intersection Observer API
+ * @param {MutableRefObject<Element>} root - Scroll parent ref
+ * @param {String} [rootMargin] - Visibility margin, in pixels or percents (default 0)
+ * @returns {Boolean} - Whether an element is visible or not
+ */
+export function useOnScreen(root, rootMargin) {
+    const [isVisible, setVisibility] = useState(false)
 
     useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                setIntersecting(entry.isIntersecting)
-            },
-            {
-                rootMargin
-            }
-        )
-        if (!ref.current) return
-        observer.observe(ref.current)
-        return () => {
-            observer.unobserve(ref.current)
-        }
-    }, [])
+        if (!root.current)
+            return
+        const observer = new IntersectionObserver(([entry]) => setVisibility(entry.isIntersecting), {rootMargin})
+        observer.observe(root.current)
+        return () => observer.unobserve(root.current)
+    }, [root.current])
 
-    return isIntersecting
+    return isVisible
 }

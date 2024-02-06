47a216ce8333ca2d968955178aa88f1ccc9b95d1
orbitlens <orbit.lens@gmail.com>
2024-02-05 23:15:08 -0100

    Expand Accordion customization

--- interaction/accordion.js
+++ interaction/accordion.js
@@ -3,27 +3,34 @@ import cn from 'classnames'
 import './accordion.scss'
 
 /**
- * @param {{title, content}[]} options
+ * Group of collapsible panels
+ * @param {{key: String, title: String|JSX.Element, content:*}[]} options - Accordion options
+ * @param {String} [collapsedSymbol] - Prefix to show for collapsed panels
+ * @param {String} [expandedSymbol] - Prefix to show for expanded panel
  */
-export function Accordion({options}) {
+export function Accordion({options, collapsedSymbol = '+', expandedSymbol = '-', ...otherProps}) {
     const [selectedOption, setSelectedOption] = useState()
     useEffect(() => {
-        setSelectedOption(options[0].title)
+        const firstOption = options[0]
+        setSelectedOption(firstOption.key || firstOption.title)
     }, [options])
-    const changeSelected = useCallback(e => {
-        const {title} = e.currentTarget.dataset
-        setSelectedOption(title)
-    }, [options])
-    return <div className="accordion">
-        {options.map(({title, content}) => <div key={title} className={cn('option', {open: selectedOption === title})}>
-            <div className="accordion-header" data-title={title} onClick={changeSelected}>
-                {title}
-            </div>
-            <div className="accordion-collapse">
-                <div className="accordion-body">
-                    {content}
+    const changeSelected = useCallback(e => setSelectedOption(e.currentTarget.dataset.key), [options])
+    return <div className="accordion" {...otherProps}>
+        {options.map(({key, title, content}) => {
+            const expanded = key ?
+                selectedOption === key :
+                selectedOption === title
+            return <div key={key || title} className={cn('option', {expanded})}>
+                <div className="accordion-header" data-collapsed={collapsedSymbol} data-expanded={expandedSymbol} data-key={key || title}
+                     onClick={changeSelected}>
+                    {title || key}
+                </div>
+                <div className="accordion-collapse">
+                    <div className="accordion-body">
+                        {content}
+                    </div>
                 </div>
             </div>
-        </div>)}
+        })}
     </div>
 }

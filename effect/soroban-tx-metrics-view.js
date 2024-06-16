2e78bb489551b48be4e03266b35580902d36194e
orbitlens <orbit.lens@gmail.com>
2024-06-16 13:53:25 +0000

    Update Soroban transaction metrics layout

--- effect/soroban-tx-metrics-view.js
+++ effect/soroban-tx-metrics-view.js
@@ -1,37 +1,40 @@
 import {formatWithPrecision} from '@stellar-expert/formatter'
+import './soroban-tx-metrics.scss'
 
 export default function SorobanTxMetricsView({metrics}) {
     if (!metrics)
         return null
-    return <div className="text-tiny row micro-space text-monospace">
-        {Object.entries(parseMetrics(metrics)).map(([key, value])=><div key={key} className="column column-33">
-            <span className="dimmed">{key}: </span>{value}
+    return <div className="soroban-tx-metrics-info row micro-space text-tiny text-monospace condensed">
+        {Object.entries(parseMetrics(metrics)).map(([key, value]) => <div key={key} className="column column-25">
+            <span className="dimmed">{key}: </span><MetricValue value={value}/>
         </div>)}
     </div>
 }
+
+const MetricValue = React.memo(function ({value}) {
+    const match = /\D+$/.exec(value)
+    if (match)
+        return <>{value.substring(0, match.index)}<span className="dimmed value-suffix">{value.substring(match.index)}</span></>
+    return <>{value}</>
+})
 
 function parseMetrics(metrics) {
     const res = {}
     for (const [key, value] of Object.entries(metrics)) {
         switch (key) {
             case 'cpu_insn':
-                res['CPU instructions'] = formatWithPrecision(value, 0)
+                res['Instructions'] = formatWithPrecision(value, 0)
                 break
             case  'emit_event':
                 if (value > 0) {
-                    res['Emitted events'] = value.toString()
-                }
-                break
-            case 'emit_event_byte':
-                if (value > 0) {
-                    res['Emitted events'] += ' ' + formatBytes(value)
+                    res['Emitted events'] = value.toString() + ' ' + formatBytes(value)
                 }
                 break
             case 'invoke_time_nsecs':
                 res['Invoke time'] = formatWithPrecision(value / 1000, 3) + 'µs'
                 break
             case 'mem_byte':
-                res['Memory utilization'] = formatBytes(value)
+                res['Memory usage'] = formatBytes(value)
                 break
             case 'read_code_byte':
                 if (value > 0) {
@@ -75,22 +78,22 @@ function parseMetrics(metrics) {
                 break
             case 'read_entry':
                 if (value > 0) {
-                    res['Entries read'] = value
+                    res['Entries read'] = value.toString()
                 }
                 break
             case 'write_entry':
                 if (value > 0) {
-                    res['Entries write'] = value
+                    res['Entries write'] = value.toString()
                 }
                 break
+            case 'emit_event_byte': //ignore these metrics
             case 'max_emit_event_byte':
             case 'max_rw_code_byte':
             case 'max_rw_data_byte':
-            case 'max_rw_key_byte': //ignore these metrics
-                break
-            case 'type':
+            case 'max_rw_key_byte':
+            case 'type': //ignore common effect props
             case 'source':
-            case 'contract': //ignore other effect params
+            case 'contract':
                 break
             default:
                 console.log('Unsupported metric: ' + key)

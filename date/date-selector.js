2e55c05adc74aa5a340c2b4b036c6a8f4494cff4
orbitlens <orbit.lens@gmail.com>
2024-08-08 20:19:34 +0000

    Normalize DateSelector onChange callback format

--- date/date-selector.js
+++ date/date-selector.js
@@ -2,19 +2,12 @@ import React, {useCallback, useEffect, useState} from 'react'
 import {debounce} from 'throttle-debounce'
 import {normalizeDate, toUnixTimestamp} from '@stellar-expert/formatter'
 
-export function trimIsoDateSeconds(date) {
-    if (typeof date === 'number') {
-        date = new Date(date)
-    }
-    return date.toISOString().replace(/:\d{2}\.\d*Z/, '')
-}
-
-const minSelectableValue = trimIsoDateSeconds(new Date('2015-09-30T16:46:00Z'))
-
 /**
- * @param {String} value
- * @param {Function} onChange
- * @constructor
+ * @param {String} value - Current input value
+ * @param {DateSelector~OnChange} onChange - Onchange event handler
+ * @param {String} [min] - Minimum allowed date
+ * @param {String} [max] - Maximum allowed date
+ * @param {*} [ref]
  */
 export function DateSelector({value, onChange, min, max, ref, ...otherProps}) {
     const [date, setDate] = useState(value ? trimIsoDateSeconds(normalizeDate(value)) : '')
@@ -30,7 +23,7 @@ export function DateSelector({value, onChange, min, max, ref, ...otherProps}) {
         if (value !== newDate) {
             setDate(newDate)
             if (onChange) {
-                onChange(newDate ? toUnixTimestamp(normalizeDate(newDate)) : '')
+                onChange(newDate ? toUnixTimestamp(normalizeDate(newDate)) : null)
             }
         }
     }), [onChange, value])
@@ -42,3 +35,21 @@ export function DateSelector({value, onChange, min, max, ref, ...otherProps}) {
                   min={min || minSelectableValue} max={max} onChange={e => selectDate(e.target.value)}
                   style={{width: '11em', overflow: 'hidden'}} {...otherProps}/>
 }
+
+/**
+ * @param {Date|Number} date
+ * @return {String}
+ */
+export function trimIsoDateSeconds(date) {
+    if (typeof date === 'number') {
+        date = new Date(date)
+    }
+    return date.toISOString().replace(/:\d{2}\.\d*Z/, '')
+}
+
+const minSelectableValue = trimIsoDateSeconds(new Date('2015-09-30T16:46:00Z'))
+
+/**
+ * @callback DateSelector~OnChange
+ * @param {number|null} value - Selected value represented as a Unix timestamp or null for invalid date value
+ */
